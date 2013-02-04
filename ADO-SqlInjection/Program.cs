c506c219d8a973376c4e1d0f79ca80b9177eb088
Alexandre Engelhardt <alexandre@zipion-live.be>
2013-02-04 12:30:37 +0100

    Ajout d'un exemple de connection sécurisée
    Avec utilisation de paramètres SQL

--- ADO-SqlInjection/Program.cs
+++ ADO-SqlInjection/Program.cs
@@ -20,13 +20,32 @@ namespace ADO_SqlInjection
 
                 string login = "glucas' --";
                 string lastName = "Mais pas Lucas";
-                string injection = string.Format("SELECT section_id FROM Student WHERE login = '{0}' AND last_name = '{1}';", login, lastName);
+                string injection = string.Format("SELECT section_id FROM Student WHERE login = '{0}' AND last_name = '{1}';", login, lastName); // une requête concaténée comme celle-ci est vulnérable aux injections
 
                 SqlCommand cmd = db.CreateCommand();
                 cmd.CommandText = injection;
 
                 db.Open();
                 object returnvalue = cmd.ExecuteScalar();
+                db.Close();
+
+                Console.WriteLine(returnvalue.ToString());
+
+                // méthode sécurisée avec paramètres SQL
+                string noInjection = "SELECT section_id FROM Student WHERE login = @login AND last_name = @lName";
+
+                SqlParameter paramLogin = new SqlParameter();
+                paramLogin.ParameterName = "@login";
+                paramLogin.Value = login;
+
+                SqlParameter paramLName = new SqlParameter();
+                paramLName.ParameterName = "@lName";
+                paramLName.Value = lastName;
+
+                cmd.CommandText = noInjection;
+
+                db.Open();
+                object returnvalue2 = cmd.ExecuteScalar();
                 db.Close();
 
                 Console.WriteLine(returnvalue.ToString());
