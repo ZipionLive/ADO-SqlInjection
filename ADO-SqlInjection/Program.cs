a191a8412e45180da8c7102dd50617e7b07c7bd0
Alexandre Engelhardt <alexandre@zipion-live.be>
2013-02-04 12:07:10 +0100

    Injection réussie

---
+++ ADO-SqlInjection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace ADO_SqlInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectString = @"Data Source = ASPIRE-5560G\ZLSQL; Initial Catalog = DBSlides; Integrated Security = True";
+
+            try
+            {
+                SqlConnection db = new SqlConnection();
+                db.ConnectionString = connectString;
+
+                string login = "glucas' --";
+                string lastName = "Mais pas Lucas";
+                string injection = string.Format("SELECT section_id FROM Student WHERE login = '{0}' AND last_name = '{1}';", login, lastName);
+
+                SqlCommand cmd = db.CreateCommand();
+                cmd.CommandText = injection;
+
+                db.Open();
+                object returnvalue = cmd.ExecuteScalar();
+                db.Close();
+
+                Console.WriteLine(returnvalue.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
